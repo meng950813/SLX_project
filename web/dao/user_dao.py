17d4477036518e43ee39d3fd814ad471eb355e34
meng950813 <1752397568@qq.com>
2019-07-05 11:13:02 +0800

    by chen: 允许用户使用id/电话/邮箱登陆

--- web/dao/user_dao.py
+++ web/dao/user_dao.py
@@ -1,9 +1,43 @@
 from web.utils import mysql
 
 
-def select(username):
-    # 把密码转换成hash
-    sql = "select * from user where tel_number = ?"
-    result = mysql.select_one(sql, username)
+
+def dologin(telphone = None, email = None , u_id = None , pwd = ""):
+    """
+    根据 账号信息 及 密码 查询用户信息
+    :param telphone: 手机号
+    :param email: 邮箱
+    :param u_id: 用户id
+    :param pwd: 密码（32位字符串）
+    :return: 用户信息 ： （ID,NAME,TYPE）or None
+    """
+    sql_base = "select ID,NAME,TYPE from sys_user where "
+    argu = None
+    if telphone:
+        sql_base += "TEL_NUMBER = ?"
+        argu = telphone
+    elif email:
+        sql_base += "EMAIL = ?"
+        argu = email
+    elif u_id:
+        sql_base += "ID = ?"
+        argu = u_id
+    else:
+        print("error arugments")
+        return  None
+    sql_base += " and PASSWORD = ?"
+    # 调用语句
+    result = mysql.select_one(sql_base, argu, pwd )
 
     return result
+
+if __name__ == "__main__":
+    # 测试：
+    # success with telphone
+    print(dologin(telphone = "12345678901", pwd = "3b86247f12fa88a116e8e446614b3eae"))
+    # success with email
+    print(dologin(email= "c@m.com", pwd = "3b86247f12fa88a116e8e446614b3eae"))
+    # success with user_id
+    print(dologin(u_id= "100000", pwd = "3b86247f12fa88a116e8e446614b3eae"))
+    # fail to login
+    print(dologin(telphone = "12345678901", pwd = "3b86247f12fa88a116e8e"))
--- web/service/user_service.py
+++ web/service/user_service.py
@@ -1,15 +1,34 @@
 from web.dao import user_dao
 from web.utils import encrypt
+import re
 
 
 def check_user(username, password):
-    user = user_dao.select(username)
+    """
+    根据账户名密码
+    :param username:用户名（str）
+    :param password: 原始密码（str）
+    :return:  字典对象{'ID': 1, 'NAME': '测试账号', 'TYPE': '1'}
+            or None
+    """
+    tel = re.compile(r"^\d{11}$")
+    email = re.compile(r"^[\w\d]+@[\w\d]+\.com$")
+    u_id = re.compile(r"^\d{6,8}$")
+    #  加密出密码
+    password = encrypt.encryption(password)
 
-    if user is None:
-        return None
-    password_hash = encrypt.encryption(password)
+    back = None
 
-    # 数据存放到session中？
-    if password_hash == user['password']:
-        return user
-    return None
+    # 利用正则判断用户名是否符合标准，以减少 sql 注入可能性
+    if tel.match(username) :
+        # 以电话登陆
+        back = user_dao.dologin(telphone = username , pwd = password)
+    elif email.match(username):
+        # 以邮件登陆
+        back = user_dao.dologin(email = username , pwd = password)
+    elif u_id.match(username):
+        # 以 id 登陆
+        back = user_dao.dologin(u_id = username , pwd = password)
+    
+    # 返回查询结果
+    return back
