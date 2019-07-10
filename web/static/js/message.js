ed991ff03529a8c13a7d2a75c8ad1932390554e1
df <1148705128@qq.com>
2019-07-10 18:50:39 +0800

    by董峰，添加增加发送消息的js函数

--- web/static/js/message.js
+++ web/static/js/message.js
@@ -1,20 +1,11 @@
 /**
-* 针对修改不同版块，修改模态框的标题
-*/
-$("#send_message").on("click", (e)=> {
-    let $receivce= $("#receiver").val();
-    let $content =$("#content").val();
-    
-
-});
-
-/**
  * 发送消息
  * @param element
  */
-function send_message(element){
+$("#send_message").click((e) =>{
     let receiver= $("#receiver").val();
     let content = $("#content").val();
+    console.log(receiver)
     $.ajax({
         url: '/add_message',
         type: 'POST',
@@ -32,32 +23,5 @@ function send_message(element){
             $('#total').text(parseInt($('#total').text()) - 1);
         }
     });
-}
+});
 
-/**
- * 删除拜访记录
- * @param element
- */
-function deleteRecord(element) {
-    if (!confirm('确定删除此条记录?'))
-        return;
-    let record_id = element.parent().parent('.operation').find('#identifier').val();
-    let csrf_token = element.parent().find('#csrf_token').val();
-    //发送事件
-    $.ajax({
-        url: '/visit_record/delete',
-        type: 'POST',
-        data: {
-            id: record_id,
-            csrf_token: csrf_token,
-        },
-        dataType: 'json'
-    }).done(function (data) {
-        if(data.success){
-            toggle_alert(true, "", "删除成功");
-            //删除此条记录
-            element.parents('tr').remove();
-            $('#total').text(parseInt($('#total').text()) - 1);
-        }
-    });
-}
