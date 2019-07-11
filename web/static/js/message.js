9a5da6a27cd6a68ec7d8963193f79f2e954ceb0a
sky94520 <541052067@qq.com>
2019-07-11 10:40:09 +0800

    by xiaoniu 添加了自动补全功能

--- web/static/js/message.js
+++ web/static/js/message.js
@@ -7,8 +7,19 @@ $("#send_message").click((e) =>{
     let content = $("#content").val();
     let csrf_token = $("#csrf_token").val();
     //TODO:接收者的id已经得知
-    let receiver_id = 100000;
-    console.log(receiver);
+    let receiver_id = null;
+    //TODO:遍历查找接收者的id 目前未处理存在重名的情况
+    for (let i = 0; i < users.length; i++){
+        if (users[i].name == receiver){
+            receiver_id = users[i].id;
+            break;
+        }
+    }
+    if (receiver_id == null)
+    {
+        toggle_alert(false, "", "未找到对应的人，请确定名字后重试");
+        return ;
+    }
     $.ajax({
         url: '/add_message',
         type: 'POST',
@@ -25,4 +36,21 @@ $("#send_message").click((e) =>{
         }
     });
 });
+
+//请求获取所有的商务名字和id
+let users = null;
+$.ajax({
+    url: '/get_agents',
+    type: 'GET',
+    dataType: 'json'
+}).done(function (data) {
+    users = data;
+    let names = [];
+    //自动补全
+    for (let i = 0; i < users.length; i++)
+        names.push(users[i].name);
+    /*传递参数*/
+    autocomplete(document.getElementById("receiver"), names);
+});
+
 
