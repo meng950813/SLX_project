9240d3e17832a072c59dce2aaff29a48104febf0
meng950813 <1752397568@qq.com>
2019-07-11 16:49:42 +0800

    by chen: 将关于教师详情的操作从school_agent中独立到scholar_detail中

--- web/blueprints/school_agent.py
+++ web/blueprints/school_agent.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template, session, request
-from web.service.basic_info_service import search_teacher_basic_info
 import json
 import os
-import datetime
 
 from web.blueprints.auth import login_required
 from web.utils.mongo_operator import MongoOperator
@@ -79,62 +77,6 @@ def change_institution():
         return json_data
     else:
         return json.dumps({"success": False, "message": "暂无当前学院的社交网络数据"})
-
-
-@school_agent_bp.route('/scholar/<int:teacher_id>')
-@login_required
-def scholar_info(teacher_id):
-    """
-    专家个人信息
-    :param teacher_id:
-    :return:
-    """
-    # 返回json 序列化后的文件
-    teacher_basic_info = search_teacher_basic_info(teacher_id)
-    length = [0, 0]
-
-    # 计算该教师所拥有的基金的数目，并将其加到列表中，用以传送给前端
-    if "funds" in teacher_basic_info:
-        length[0] = len(teacher_basic_info["funds"])
-
-    # 计算该教师所拥有的专利
-    if "patent" in teacher_basic_info:
-        length[1] = len(teacher_basic_info["patent"])
-
-    # 计算教师年龄
-    birth_year = teacher_basic_info["birth_year"]
-    if birth_year == " ":
-        age = " "
-    else:
-        year = datetime.datetime.now().year
-        age = year - int(birth_year)
-
-    return render_template('detail.html', teacher_basic_info=teacher_basic_info, length=length, age=age)
-
-
-@school_agent_bp.route('/info_modify', methods=['POST'])
-@login_required
-def info_modify():
-    """
-    进行信息修改
-    :return:
-    """
-    # TODO 修改函数名 及 在数据库中存储的结构
-    info = {
-        'title': request.form.get('title'),
-        'type': request.form.get('type'),
-        'target': request.form.get('target'),
-        'content': request.form.get('content')
-    }
-    mongo_operator = MongoOperator(**MongoDB_CONFIG)
-    # 获取用户的uid
-    uid = session['uid']
-    mongo_operator.db['info_modify'].insert_one(
-        {
-            "user_id": uid,
-            "info_modify": info
-        })
-    return json.dumps({'success': True})
 
 
 def get_institution(school):
