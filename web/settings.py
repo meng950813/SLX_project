aafafeccbc56fd2b7fc56bda14139cebb7fe31d8
sky94520 <541052067@qq.com>
2019-07-16 16:22:57 +0800

    by xiaoniu 添加了邮箱的配置

--- web/settings.py
+++ web/settings.py
@@ -1,4 +1,5 @@
 import os
+from web.config import MAIL_CONFIG
 
 
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
@@ -7,6 +8,13 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 class BaseConfig(object):
     # wtform库用于CSRF
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev key')
+    # 邮件相关
+    MAIL_SERVER = MAIL_CONFIG['server']
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = MAIL_CONFIG['username']
+    MAIL_PASSWORD = MAIL_CONFIG['password']
+    MAIL_DEFAULT_SENDER = ('三螺旋', MAIL_CONFIG['sender'])
 
 
 class DevelopmentConfig(BaseConfig):
