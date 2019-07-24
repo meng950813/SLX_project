9564a0b74a9456f1dd07789ce3e52e6c4063baa3
coke_1996 <18796360983zs@gmail.com>
2019-07-24 10:43:30 +0800

    by zhang : 更改添加拜访记录中出现的bug，同时由xiaoniu改了新增记录时的id显示bug

--- web/blueprints/school_agent.py
+++ web/blueprints/school_agent.py
@@ -66,6 +66,27 @@ def change_school():
     return json.dumps({"success": False, "message": "学校名有误"})
 
 
+@school_agent_bp.route('/get_schools', methods=['GET'])
+@login_required
+def get_schools():
+    """
+    by zhang
+    获取所有学校的名称组成的列表
+    :return:
+    """
+    mongo = MongoOperator(**MongoDB_CONFIG)
+    try:
+        school_col = mongo.get_collection("school")
+        schools = list(school_col.find({}, {"_id": 0, "name": 1}))
+
+        # 转成学校列表
+        school_list = [d["name"] for d in schools]
+        return json.dumps({"success": True, "school_list": school_list})
+
+    except:
+        return json.dumps({"success": True, "school_list": []})
+
+
 @school_agent_bp.route('/change_institution', methods=["GET", "PUT"])
 @login_required
 def change_institution():
@@ -488,12 +509,12 @@ def change_pwd_in_db(new_pwd):
         return json.dumps({"success": False})
 
 
-if __name__ == '__main__':
-    # scholar_info(73927)
-    print(get_relations("北京大学", "化学生物学与生物技术学院", ""))
-    # new_schedule()
-    # index()
-    # edit_schedule()
-    # set_whether_completed(100006,1,1)
-    get_institutions_list("清华大学")
-    # pass
+# if __name__ == '__main__':
+#     # scholar_info(73927)
+#     print(get_relations("北京大学", "化学生物学与生物技术学院", ""))
+#     # new_schedule()
+#     # index()
+#     # edit_schedule()
+#     # set_whether_completed(100006,1,1)
+#     get_institutions_list("清华大学")
+#     # pass
