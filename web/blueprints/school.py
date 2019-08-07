98b2c005e173c2db41a6dec837a3ac4eee1a0fc4
sky94520 <541052067@qq.com>
2019-08-07 09:45:59 +0800

    by xiaoniu 当学院不存在或团队不存在返回404

--- web/blueprints/school.py
+++ web/blueprints/school.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, render_template,request
+from flask import Blueprint, render_template, request, abort
 from flask_login import login_required
 
 import web.service.school as school_service
@@ -45,12 +45,12 @@ def get_institution_info():
     """
     school = request.args.get("school")
     data = school_service.get_institution_info(school)
-    return json.dumps({"success":True,"data":data})
+    return json.dumps({"success": True, "data": data})
 
 
 @school_bp.route('/<school>/<institution>')
 @login_required
-def institution(school, institution):
+def show_institution(school, institution):
     """
     显示某一学校的学院的相关信息 主要是院系中的团队 关系图
     :param school:
@@ -62,6 +62,9 @@ def institution(school, institution):
     collection = mongo.get_collection('institution')
     result = collection.find_one({'school': school, 'institution': institution},
                                  {'_id': 0, 'school': 0, 'institution': 0})
+    # 结果为空，404
+    if result is None:
+        abort(404)
     keys = [('academician_num', '院士'), ('cjsp_num', '长江学者'), ('dfc_num', '一流学科'),
             ('nkd_num', '重点学科'), ('outstanding_num', '杰出青年'), ('skl_num', '重点实验室')]
     objects = []
@@ -84,6 +87,13 @@ def show_team(school, institution, team_index):
     :return:
     """
     graph_data = school_service.get_team(school, institution, team_index)
+    # 学校或者是学院不存在
+    if graph_data is False:
+        abort(404)
+    # 团队不存在
     core_node = graph_data['core_node']
+    if len(core_node) == 0:
+        abort(404)
+
     return render_template('school/team.html', school=school, institution=institution,
                            graph_data=json.dumps(graph_data), core_node=core_node)
